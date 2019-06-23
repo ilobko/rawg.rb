b52ae2d304c019d382d462581145d110250cafe6
Ivan Lobko <11068931+ivanlobko@users.noreply.github.com>
2019-06-23 17:14:21 +0300

    Add new empty specs to Game

--- spec/rawg/game_spec.rb
+++ spec/rawg/game_spec.rb
@@ -10,9 +10,32 @@ describe RAWG::Game do
   it { is_expected.to have_attr_accessor(:released) }
   it { is_expected.to have_attr_accessor(:website) }
   it { is_expected.to have_attr_accessor(:rating) }
-  it 'yields itself' do
-    yielded_instance = nil
-    new_instance = described_class.new { |i| yielded_instance = i }
-    expect(yielded_instance).to be new_instance
+  
+  describe '#initialize' do
+    context 'when client is specified' do
+      it 'uses that client'
+    end
+    
+    context 'when client is not specified' do
+      it 'uses RAWG::Client.new'
+    end
+    
+    context 'when block given' do
+      it 'yields itself' do
+        yielded_instance = nil
+        new_instance = described_class.new { |i| yielded_instance = i }
+        expect(yielded_instance).to be new_instance
+      end
+    end
+  end
+
+  describe '#from_api_response' do
+    it 'returns self'
+    it 'assigns attributes from response'
+  end
+
+  describe '#suggested' do
+    it 'makes a request to /games/%{id}/suggested'
+    it 'returns a collection of games'
   end
 end
