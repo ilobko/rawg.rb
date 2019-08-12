91562dbd507c047dc3f90ccc8a58c1bf3405d3b0
Ivan Lobko <11068931+ivanlobko@users.noreply.github.com>
2019-08-12 14:28:23 +0300

    Use new Faker::Date syntax

--- spec/factories/game_factory.rb
+++ spec/factories/game_factory.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
     slug { Faker::Internet.slug }
     name { Faker::App.name }
     description { Faker::Lorem.paragraph }
-    released { Faker::Date.backward(1000) }
+    released { Faker::Date.backward(days: 1000) }
     website { Faker::Internet.domain_name }
     rating { rand(0.0...5.0).round(2) }
   end
